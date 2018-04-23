9d6897c7e058b595c84187aa45c9e0d03de1793d
Jesse Chan <jc@lineageos.org>
2018-04-23 13:53:27 -0700

    star: add AOSP support
    Change-Id: If5b8511ccaa3ed8a01277a65edb500e19b9ddcea

---
+++ aosp_star2lte.mk
@@ -0,0 +1,6 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
+
+PRODUCT_NAME := aosp_star2lte
