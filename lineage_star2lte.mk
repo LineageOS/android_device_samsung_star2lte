4527f66fd945782d2fad3246d8c2c1247c29a858
synt4x93 <synt4x.93@gmail.com>
2019-06-23 01:33:30 +0000

    star2lte: Add fingerprint override
    Change-Id: Ia32b23c4f71f13be10811c3c95e753abca087990

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -7,3 +7,8 @@ $(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_star2lte
+
+BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:10/QP1A.190711.020/G965FXXU8DTC5:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="star2ltexx-user 10 QP1A.190711.020 G965FXXU8DTC5 release-keys"
