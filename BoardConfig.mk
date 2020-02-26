15e2aebfaefaafe18b65151c4100a728f2f01607
Erfan Abdi <erfangplus@gmail.com>
2020-02-26 15:55:13 +0330

    star2lte: Add system partition size
    Change-Id: I386d113506deb44dd53057eeff0b3f08abdeb2f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,8 @@ TARGET_OTA_ASSERT_DEVICE := star2ltexx,star2lteks,star2lte
 # Kernel
 TARGET_KERNEL_CONFIG := exynos9810-star2lte_defconfig
 
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 4561305600
+
 # Inherit common board flags
 include device/samsung/universal9810-common/BoardConfigCommon.mk
